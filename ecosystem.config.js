206ef24ccd61865761cfd005dce0638066933641
marcell88 <marcell.davydov@gmail.com>
2025-11-01 11:20:06 +0300

    script added

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps: [{
+        name: 'app',
+        script: 'app.py',
+        interpreter: 'venv/bin/python',
+        cwd: '/home/markell/tg-bot-news-parser'
+    }]
+}
